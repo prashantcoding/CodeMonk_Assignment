40903501de3bf1e4d416992a45b975f3ee78f2ab
prashantcoding <soultaker399@gmail.com>
2022-12-01 20:46:42 +0530

    building Navbar

---
+++ src/Pages/ShopCart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ShopCart.css";
+const ShopCart = () => {
+  return (
+    <div className="shop__container">
+      <p className="shop__heading">Shopping cart</p>
+    </div>
+  );
+};
+
+export default ShopCart;
